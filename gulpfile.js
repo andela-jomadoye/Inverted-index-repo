fc3154aa44e5038d5b7984428fa8495489e1a431
Jedidiah Omadoye <jedidiah.omadoye@andela.com>
2017-03-27 13:33:59 +0100

    fix codeclimate

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var bs = require('browser-sync')
+const gulp = require('gulp');
+const bs = require('browser-sync')
   .create(); // create a browser sync instance.
 
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', () => {
   bs.init({
     server: {
       baseDir: "."
@@ -10,14 +10,14 @@ gulp.task('browser-sync', function () {
   });
 });
 
-gulp.task('watch', function () {
-  gulp.watch("*.html")
+gulp.task('watch', () => {
+  gulp.watch('*.html')
     .on('change', bs.reload);
-  gulp.watch("public/js/*.js")
+  gulp.watch('public/js/*.js')
     .on('change', bs.reload);
-  gulp.watch("jasmine/**/*.js")
+  gulp.watch('jasmine/**/*.js')
     .on('change', bs.reload);
-  gulp.watch("public/css/*.css")
+  gulp.watch('public/css/*.css')
     .on('change', bs.reload);
 });
 
