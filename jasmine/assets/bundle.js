9e4a803a7126f0b85c92ebfde8680ee30aa5b4f1
Jedidiah Omadoye <jedidiah.omadoye@andela.com>
2017-03-28 17:10:52 +0100

    chore(jasmine) setup browserify

--- jasmine/assets/bundle.js
+++ jasmine/assets/bundle.js
@@ -1,3 +1,59 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports=[
+  {
+    "title": "Alice in New Wonderland",
+    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+  },
+
+  {
+    "title": "The Lord of the Rings: The New Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  },
+
+  {
+    "title": "The Lord of the Rings: The New Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  },
+  {
+    "title": "The Lord of the Rings: The New Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  }
+]
+},{}],2:[function(require,module,exports){
+module.exports={}
+},{}],3:[function(require,module,exports){
+module.exports=[
+  {
+    "title": "Alice in New Wonderland",
+    "text": "Alice### falls @@into &&a ^^^rabbit $$"
+  }
+]
+},{}],4:[function(require,module,exports){
+module.exports=[
+  {
+    "title": "Alice in New Wonderland",
+    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+  },
+
+  {
+    "title": "The Lord of the Rings: The New Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  },
+
+  {
+    "title": "The Lord of the Rings: The New Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  },
+  {
+    "title": "The Lord of the Rings: The New Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  },
+  {
+    "title": "The Lord of the Rings: The New Fellowship of the Ring.",
+    "textd": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  }
+]
+},{}],5:[function(require,module,exports){
 const uniqueTermsBook = ['alice', 'falls', 'into', 'a'];
 const books = require('../assets/books.json');
 const wrongdata = require('../assets/wrongdata.json');
@@ -158,3 +214,5 @@ describe('InvertedIndex Class', () => {
     });
   });
 });
+
+},{"../assets/books.json":1,"../assets/emptyfile.json":2,"../assets/smallfile.json":3,"../assets/wrongdata.json":4}]},{},[5]);
